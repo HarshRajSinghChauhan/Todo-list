b561055127d654a2ca1914264e1600b294c630be
Harsh Raj Singh Chauhan <harshraj.singhchauhan09@gmail.com>
2025-09-18 00:52:47 +0530

    Added Deletion functionality in Todo List

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
-import './Todo.css'
-
+import { useState } from 'react';
+import './Todo.css';
+import { v4 as uuidv4} from "uuid";
 export default function Todo() {
 
     let [todos, setTodos] = useState([]);
     let [newTodo, setNewTodo] = useState("")
     
     let addTask = () => {
-        setTodos([...todos, newTodo]);
+        setTodos([...todos, {task: newTodo, id: uuidv4()}]);
         setNewTodo("")
     }
 
     let updateTodoValue = (event) =>{
         setNewTodo(event.target.value);
     }
+
+    let deleteTodo = (id) =>{
+        setTodos((prevTodos) => prevTodos.filter((todos) => todos.id != id)); 
+    }
     return (
         <div className="container">
-            <h1>TODO LIST</h1>
+            <h3>TODO LIST</h3>
             <div className='input'>
                 <input type="text" placeholder="Enter the task" value={newTodo} onChange={updateTodoValue}/>
                 <button onClick={addTask}>Add</button>
             </div>
             <ul>
                 {
-                    todos.map((todo) =>{
-                        return <li>{todo}</li>
-                    })
+                    todos.map((todo) =>(
+                        <li key={todo.id}>
+                            <span>{todo.task}</span> 
+                            &nbsp; &nbsp; &nbsp; &nbsp;
+                            <button id="editBtn">Edit</button>
+                            &nbsp; &nbsp; &nbsp; &nbsp;
+                            <button id ='dltBtn' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        </li>
+                    ))
                 }
             </ul>
         </div>
