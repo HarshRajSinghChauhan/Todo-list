e0d237691ef09e1e8fabb823c5401c601547084b
Harsh Raj Singh Chauhan <harshraj.singhchauhan09@gmail.com>
2025-09-18 16:04:50 +0530

    Added 'Mark As Done' Feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,39 +1,62 @@
 import { useState } from 'react';
 import './Todo.css';
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 export default function Todo() {
 
     let [todos, setTodos] = useState([]);
     let [newTodo, setNewTodo] = useState("")
-    
+
     let addTask = () => {
-        setTodos([...todos, {task: newTodo, id: uuidv4()}]);
+        if (newTodo.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+        setTodos([...todos, { task: newTodo, id: uuidv4(), isDone: false }]);
         setNewTodo("")
     }
 
-    let updateTodoValue = (event) =>{
+    let updateTodoValue = (event) => {
         setNewTodo(event.target.value);
     }
 
-    let deleteTodo = (id) =>{
-        setTodos((prevTodos) => prevTodos.filter((todos) => todos.id != id)); 
+    let deleteTodo = (id) => {
+        setTodos((prevTodos) => prevTodos.filter((todos) => todos.id != id));
     }
+
+    let markDone = (id) => {
+        setTodos((todos) => (
+            todos.map((todo) => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        isDone: true,
+                    }
+                }else{
+                    return todo;
+                }
+            })
+        ))
+    }
+
+
     return (
         <div className="container">
             <h3>TODO LIST</h3>
             <div className='input'>
-                <input type="text" placeholder="Enter the task" value={newTodo} onChange={updateTodoValue}/>
+                <input type="text" placeholder="Enter the task" value={newTodo} onChange={updateTodoValue} />
                 <button onClick={addTask}>Add</button>
             </div>
             <ul>
                 {
-                    todos.map((todo) =>(
+                    todos.map((todo) => (
                         <li key={todo.id}>
-                            <span>{todo.task}</span> 
+                            <span style={ todo.isDone ? {textDecoration : "Line-through"} : {}}>{todo.task}</span>
                             &nbsp; &nbsp; &nbsp; &nbsp;
                             <button id="editBtn">Edit</button>
                             &nbsp; &nbsp; &nbsp; &nbsp;
-                            <button id ='dltBtn' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                            <button id='markBtn' onClick={() => markDone(todo.id)}>Mark Done</button>
+                            &nbsp; &nbsp; &nbsp; &nbsp;
+                            <button id='dltBtn' onClick={() => deleteTodo(todo.id)}>Delete</button>
                         </li>
                     ))
                 }
